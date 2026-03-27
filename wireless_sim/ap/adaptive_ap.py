34418a85765028abfe02df97aafab149e4b0e440
devanjali0911 <devanjali0911@gmail.com>
2026-03-26 20:18:15 -0400

    Add collision handling logic to adaptive AP and standard AP functions

--- wireless_sim/ap/adaptive_ap.py
+++ wireless_sim/ap/adaptive_ap.py
@@ -6,5 +6,30 @@ from .mac_filter_ap import mac_filter_ap
 
 
 class adaptive_ap(mac_filter_ap):
-    def receive(self, pkt, collided, timestamp):
-        pass
+   def __init__(self, nodes):
+        super().__init__()
+        self.nodes = nodes
+        self.collision_window = 3000
+        self.collision_timestamps = []
+        self.collision_threshold = 10
+
+def receive(self, pkt, collided, timestamp):
+        if collided:
+            self.collision_timestamps.append(timestamp)
+
+        # sliding window for collisions
+        self.collision_timestamps = [
+            t for t in self.collision_timestamps
+            if timestamp - t <= self.collision_window
+        ]
+
+        if len(self.collision_timestamps) > self.collision_threshold:
+            self._increase_mcs()
+            self.collision_timestamps.clear()
+
+        super().receive(pkt, collided, timestamp)
+
+def _increase_mcs(self):
+        for node in self.nodes:
+            node.update_mcs(node.mcs + 1)
+        # print("[MCS INCREASE]")
